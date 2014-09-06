2ef9513434b974922b1aa782e07ff610f8cab61a
unknown <cedced19@gmail.com>
2014-09-06 09:51:11 +0200

    0.0.5

--- dist/vendor/js/client.js
+++ dist/vendor/js/client.js
@@ -1,14 +1,18 @@
 var socket = io.connect(window.location.host);
-var textarea = $('textarea');
+var $textarea = $('textarea');
+var textarea = document.getElementById('markdown')
 
 function initTextArea () {
-    document.getElementById('markdown').addEventListener('keyup', function(event) {
-        socket.emit('change', {before: textarea.val()});
+    textarea.addEventListener('keyup', function(event) {
+        socket.emit('change', {before: $textarea.val()});
     }, false)
 }
 
 socket.on('change', function(data){
-        textarea.val(data.before);
+        var start = getPositionStart(),
+        end = getPositionEnd();
+        $textarea.val(data.before);
+        setPosition(start, end);
         $('#preview').html(data.after);
 });
 
@@ -24,16 +28,52 @@ function initPrint () {
 function initReset () {
   var button = document.getElementById('reset');
   button.addEventListener('click', function(event) {
-    textarea.val('');
+    $textarea.val('');
     socket.emit('change', {before: textarea.val()});
   });
+}
+
+function getPositionStart () {
+  if ( typeof textarea.selectionStart != 'undefined' ){
+    return textarea.selectionStart;
+  }
+  // IE Support
+  textarea.focus();
+  var range = textarea.createTextRange();
+  range.moveToBookmark(document.selection.createRange().getBookmark());
+  range.moveEnd('character', textarea.value.length);
+  return textarea.value.length - range.text.length;
+}
+
+function getPositionEnd () {
+  if ( typeof textarea.selectionEnd != 'undefined' ){
+    return textarea.selectionEnd;
+  }
+  // IE Support
+  textarea.focus();
+  var range = textarea.createTextRange();
+  range.moveToBookmark(document.selection.createRange().getBookmark());
+  range.moveStart('character', - textarea.value.length);
+  return range.text.length;
+}
+
+function setPosition (end, start) {
+  end = end || start;   textarea.focus();
+    if (textarea.setSelectionRange){
+      textarea.setSelectionRange(start, end);
+    } else if (document.selection) {
+      var range = textarea.createTextRange();
+      range.moveStart('character', start);
+      range.moveEnd('character', - textarea.value.length + end);
+      range.select();
+    }
 }
 
 
 function init() {
   $.getJSON('/data', function (data) {
       if (data != null){
-        textarea.val(data.before);
+        $textarea.val(data.before);
         $('#preview').html(data.after);
       }
   });
